1f418d03bb562769039fa70b0586aef9d79b9c7b
Cellri131 <86295186+Cellri131@users.noreply.github.com>
2025-04-28 09:11:57 +0200

    Add files via upload

---
+++ pages/notre-asso.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Notre association - UVC</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/banner.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/pages.css">
+	<!-- Script -->
+	<link rel="dns-prefetch" href="https://fonts.googleapis.com">
+	<!-- CEO -->
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="(UVC) Une Vie de Chien !">
+	<meta property="og:url" content="https://une-vie-de-chien.top-films.cf/">
+	<meta name="description" content="UNE VIE DE CHIEN ! - Pour nos animaux préférés !">
+	<meta property="og:description" content="UNE VIE DE CHIEN ! - Pour nos animaux préférés !">
+	<meta property="og:image" content="/assets/img/logo-UVC-mini.webp">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@UVC">
+	<meta name="twitter:title" content="(UVC) Une Vie de Chien !">
+	<meta name="twitter:description" content="UNE VIE DE CHIEN ! - Pour nos animaux préférés !">
+	<meta name="twitter:creator" content="@UVC">
+	<meta name="twitter:image" content="/assets/img/logo-UVC-mini.webp">
+	<!-- PWA -->
+	<link rel="shortcut icon" href="/assets/img/logo-UVC-mini.webp">
+	<link rel="apple-touch-icon" href="/assets/img/logo-UVC-mini.webp">
+	<link rel="apple-touch-icon-precomposed" href="/assets/img/logo-UVC-mini.webp">
+	<meta name="theme-color" content="#ffffff">
+	<meta name="apple-mobile-web-app-status-bar" content="#ffffff">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+	<nav>
+		<ul class="nav-center">
+			<li class="nav-item"><a href="/notre-association/#quest-une-asso">Qu'est qu'une association</a></li>
+			<li class="nav-item"><a href="/notre-association/#conditions-creation">Conditions de création</a></li>
+			<li class="nav-logo">
+				<a href="/accueil/">
+					<div class="logo-UVC"></div>
+				</a>
+			</li>
+			<li class="nav-item"><a href="/notre-association/#notre-association">Notre association</a></li>
+			<li class="nav-item"><a href="/notre-association/#pourquoi-protection-animal">Pourquoi la protection animal</a></li>
+		</ul>
+		<link rel="stylesheet" type="text/css" href="/assets/css/nav.css">
+		<script type="text/javascript" src="/assets/js/nav.js"></script>
+	</nav>
+
+	<section class="banner" id="quest-une-asso">
+		<div class="banner-wrapper">
+			<div class="banner-flex_col banner-img banner__img--vertical">
+				<picture>
+					<source src="/assets/img/comment-bien-choisir-un-chien-a-adopter.jpg" type="image/jpeg">
+					<img src="/assets/img/comment-bien-choisir-un-chien-a-adopter.jpg">
+				</picture>
+			</div>
+			<div class="banner-flex_col">
+				<div class="banner-content">
+					<h1>Qu'est qu'une association ?</h1>
+					<p>Une association est un organisme à but non lucratif. Elle regroupe des personnes volontaires qui partagent les mêmes centres d’intérêt, autour d’un projet commun. Ce n’est pas obligatoirement un organisme relevant de l’intérêt général, mais elle ne cherche jamais à réaliser des bénéfices.</p>
+					<p>Une association est régie part des lois :</p>
+					<ul>
+						<li><a href="https://www.legifrance.gouv.fr/loda/id/LEGITEXT000006069570/">Loi 1901</a> <sup>(1)</sup></li>
+						<li><a href="https://www.associations.gouv.fr/liberte-associative.html">Loi 1901</a> <sup>(2)</sup></li>
+						<li><a href="https://www.legalplace.fr/guides/association-loi-1901/">Loi 1901</a> <sup>(3)</sup></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="content-association conditions-association" id="conditions-creation">
+		<h1>Les conditions de création</h1>
+		<div class="content-wrapper">
+			<h3>Choisir un nom :</h3>
+			<ul>
+				<li>Il faut s'assurer que le nom choisie n’est pas déjà utilisé par une autre association pour pouvoir s’en servir.</li>
+				<li>Savoir si le nom de l’association choisi n’est pas protégé comme par exemple un nom de marque qui à été déposé à lNPI.</li>
+				<li>Connaître et possédait un adresse permettant d'installer les bureaux de l'association.</li>
+				<li>Suite à cela, il faut demander et avoir l'approbation du déparrtement pour que l’association soit déclarée.</li>
+				<li>Il est strictement obligatoire d’avoir des idéaux non raciste ou antisémites, aucune maltraitance envers autrui, ne faisant pas allusion à tout autre état de vie de personne de la société dans le nom ou dans les dires.</li>
+				<li>L’association doit avoir un minimum de 2 partisans pour que celle-ci soit déclarée.</li>
+				<li>De plus, si l’association est à but lucratif ou non, aucune démarche illégale ou transaction illicite n’est autorisée.</li>
+			</ul>
+		</div>
+		<div class="content-wrapper">
+			<h3>Avoir un statut d’association :</h3>
+			<p><u>Les statuts sont l’acte fondateur d’une association. Il s’agit d’un contrat signé par un minimum de 2 personnes.</u></p>
+			<p>Les statuts sont l’acte de naissance de l’association. Ils comportent les informations décrivant l'objet (ou le but) de l'association et ses règles de fonctionnement. Les statuts peuvent être complétés par un règlement intérieur.<br><br>Les éléments qui doivent figurer dans les statuts de l’association sont, <b>le titre de l’association, les conditions d’admission des membres et des règles d’organisation/fonctionnement</b>.<br>De plus, les statuts doivent respecter des règles pour pouvoir se conformer à des statuts types, les soumettre à une autorité de tutelle comme un ministère et pouvoir mentionner les services fournis.<br><br>On doit déclarer les événements ou activités comme les modifications de statuts et le changement au niveau de l’administration interne à l’association. Mais, comme nous le savons, toute les associations manquent de main d'œuvre, c’est pour cela qu’il existe un formulaire de recrutement et aussi des formations a porté de tous, l’argent récolté par les formations iront directement dans l’investissement et l’achat de matériels canin.<br>Puis, si vous souhaitez juste faire des dons, vous recevez suite à cela une réduction d'impôt ayant pour but de faire participer l'État. Si toutefois vous voulez nous aider encore plus, il existe aussi des congé de responsable d’association bénévole vous autorisant en contrepartie d’aider une association, d’avoir des congés non rémunéré.</p>
+		</div>
+		<div class="content-wrapper">
+			<h3>Financement :</h3>
+			<p>Pour financer les activités de l'association, on peut mener les actions comme accepter des dons de la part des clients/utilisateurs, pouvoir demander des subventions à l’état ou pouvoir organiser une tombola.</p>
+		</div>
+		<div class="content-wrapper">
+			<h3>Des actions pour subvenir aux besoins de l'association :</h3>
+			<ul>
+				<li>Les dons.</li>
+				<li>Les subventions.</li>
+				<li>Les événements.</li>
+				<li>L’adoption.</li>
+				<li>Faire des extras jobs.</li>
+			</ul>
+		</div>
+		<div class="content-wrapper">
+			<h3>Des Actions portées par l’association pour nos animaux :</h3>
+			<ul>
+				<li>Accueil des Chiens pendant les vacances.</li>
+				<li>Recueil des Chiens trouver.</li>
+				<li>Recherche des Chiens perdu.</li>
+				<li>Aide financière.</li>
+			</ul>
+		</div>
+	</section>
+
+	<section class="banner ligth-banner" id="notre-association">
+		<div class="banner-wrapper">
+			<div class="banner-flex_col banner-img">
+				<picture>
+					<source src="/assets/img/association-chien.jpg" type="image/jpeg">
+					<img src="/assets/img/association-chien.jpg">
+				</picture>
+			</div>
+			<div class="banner-flex_col">
+				<div class="banner-content">
+					<h1>Notre association !</h1>
+					<p>L'association <b>Une Vie de Chien</b> sous loi 1901 à pour domaine d'action la protection animal. Dans notre cas, nous nous concentrons majoritairement sur les chiens.</p>
+					<p>En France, des lois pour la protection animal existe, en voici quelque unes :</p>
+					<ul>
+						<li><a href="https://agriculture.gouv.fr/animaux-de-compagnie-mise-en-place-du-certificat-dengagement-et-de-connaissance">Loi sur la maltraitance des animaux</a> <sup>(1)</sup></li>
+						<li><a href="https://www.vie-publique.fr/loi/278249-loi-2021-lutte-contre-la-maltraitance-animale">Loi maltraitance des animaux</a> <sup>(2)</sup></li>
+						<li><a href="https://www.service-public.fr/particuliers/vosdroits/F34877">Les règles pour avoir un animal</a></li>
+						<li><a href="https://www.assemblee-nationale.fr/dyn/15/dossiers/conditions_de_vie_des_animaux">Loi sur les conditions de vie des animaux</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="content-association notre-association" id="pourquoi-protection-animal">
+		<h1>Pourquoi la protection animal ?</h1>
+		<div class="content-wrapper">			
+			<p>Notre association lutte contre toutes les maltraitances animales, nous prenons en charge un hébergement en cas d’impossibilité temporaire du propriétaire. Cette association à était construite après avoir découvert une agglomération de chien errant dans Condé-en-Normandie, suite à cela, de nombreux maîtres ayant marre que leur animaux se fassent agresser ont rejoint l’association.</p>
+		</div>
+	</section>
+
+	<?php include("../recurrents/footer.php"); ?>
+</body>
+</html>
---
+++ recurrents/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer_menu">
+	<div class="footer_menu-container">
+		<ul>
+			<li>
+				<a title="Mentions légales" href="/mentions-legales/">Mentions légales</a>
+			</li>
+			<li>
+				<a title="Statut de l'association" href="/statut-association/">Statut de l'association</a>
+			</li>
+			<li>
+				<a title="Règlement intérieur de l'association" href="/reglement-interieur-association/">Règlement intérieur de l'association</a>
+			</li>
+			<li>
+				<a title="Procès-verbal de l’assemblée générale constitutive" href="/pv-assemblee-generale-constitutive/">Procès-verbal de l’assemblée générale constitutive</a>
+			</li>
+			<li>
+				<a title="Plan du site" href="/plan-du-site/">Plan du site</a>
+			</li>
+			<li>
+				<a title="Nous rejoindre" href="/nous-rejoindre/">Nous rejoindre</a>
+			</li>
+		</ul>
+		<a title="Accueil" href="/accueil/"><div class="footer_menu-logo"></div></a>
+	</div>
+	<div class="footer_menu-container">
+		<div class="footer_menu-copyright">			
+			<p>Une Vie de Chien ! - Copyright 2023</p>
+			<p>Version site : <span>1.2.0</span></p>
+		</div>
+	</div>
+	<link rel="stylesheet" type="text/css" href="/assets/css/footer.css">
+</footer>
